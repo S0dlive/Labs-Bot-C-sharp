bbcc45d5df61f44de9b37993c0f1716b2a96ca2d
bptlcn <terrydevs0@gmail.com>
2022-03-23 13:32:26 +0100

    Bot Base

--- Labs Bot Discord/Labs Bot Discord/Program.cs
+++ Labs Bot Discord/Labs Bot Discord/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿using DSharpPlus;
+
+namespace Labs;
+
+public static class Program
+{
+    /// <summary>
+    /// Point D'entrer : D
+    /// </summary>
+    public static void Main()
+    {
+        MainAsync().GetAwaiter().GetResult(); // eheh boy 😊
+        
+    }
+
+    public static async Task MainAsync()
+    {
+        
+        var discord = new DiscordClient(new DiscordConfiguration()
+        {
+            Token = "Bien Tenter mais le token est pas ici ahah !",
+            TokenType = TokenType.Bot,
+            Intents = DiscordIntents.AllUnprivileged     
+        });
+        
+        await discord.ConnectAsync();
+        await Task.Delay(-1);
+    }
+}
